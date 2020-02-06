f2ae30883fdfb63ef6f2266935f2ab8227ae5bb6
Juan David Nunez <juandavidnh93@gmail.com>
2020-02-06 07:22:46 -0500

    ready for deploy

--- src/app.js
+++ src/app.js
@@ -21,6 +21,17 @@ app.get('/', (req, res) => {
     res.send('Hello, boilerplate!')
 })
 
+app.use(function validateBearerToken(req, res, next){
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({error: 'Unauthorized request'})
+    }
+
+    next()
+})
+
 app.use('/api/folders', foldersRouter)
 app.use('/api/notes', notesRouter)
 
